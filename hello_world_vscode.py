0efa231cd43b1b202d87cf66765995647959d5c6
jbaumer <jbaumer@ethz.ch>
2022-02-08 17:40:54 +0100

    first commit

---
+++ hello_world_vscode.py
@@ -0,0 +1,16 @@
+# notes for visual studio
+
+# (Ctrl+) F5 to (debug and) run code 
+print('Hello World!')
+
+
+# import of often used libraries work
+import pandas
+print('pandas imported sucessfully')
+
+# set variables on Watch to check their values (e.g. on breakpoints)
+x=42
+y=55
+x=41
+
+print('done')
