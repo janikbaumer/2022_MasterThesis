ae32f53a1f8a186ad032fabf90ec4b7a003ff542
jbaumer <jbaumer@ethz.ch>
2022-03-01 11:51:26 +0100

    include validation set (not yet working), training should be working

--- src/isFoggy_pretrained_featureExtractor.py
+++ src/isFoggy_pretrained_featureExtractor.py
@@ -1,24 +1,42 @@
 from time import time
+from numpy import datetime_as_string
 import torch
 from dischma_set import DischmaSet
 from torch.utils.data import DataLoader
 from binary_classifier import MyNet # , Binary_Classifier
 from torch import nn
 from torchvision import models
+from torch.utils.data import random_split
+print('imports done')
 
+torch.seed()
+torch.manual_seed(42)
+
+# set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BATCH_SIZE = 8
 LEARNING_RATE = 0.01
 EPOCHS = 3
-N_CLASSES = 1
+N_CLASSES = 2
+TRAIN_SPLIT = 0.8
 
-print('imports done')
-dset_train = DischmaSet(root='../datasets/dataset_downsampled/')
-print('dischma set created')
-dloader_train = DataLoader(dataset=dset_train, batch_size=BATCH_SIZE)
+dset_bb1 = DischmaSet(root='../datasets/dataset_downsampled/', station='Buelenberg', camera=1)
+print('dischma set created.')
 
-# set device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def get_train_val_split(dset_full):
+    print('splitting in train/test...')
+    len_full = len(dset_full)
+    len_train = int(TRAIN_SPLIT*len_full)
+    len_val = len_full - len_train
+    dset_train, dset_val = random_split(dset_bb1, [len_train, len_val])  # Split Pytorch tensor
+    return dset_train, dset_val
+
+dset_bb1_train, dset_bb1_val = get_train_val_split(dset_bb1)
+
+
+dloader_train = DataLoader(dataset=dset_bb1_train, batch_size=BATCH_SIZE)
+dloader_val = DataLoader(dataset=dset_bb1_val, batch_size=BATCH_SIZE)
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs):
@@ -36,15 +54,19 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
 
             running_loss = 0.0  # loss (to be updated during loop)
             running_corrects = 0  # correct labels (to be updated during loop)
-
+        
+        print('start looping')
+        loop = 0
+        train_loss = 0
         for x, y in dloader_train:  # x,y are already moved to device in dataloader
-            x = x.to(device)
-            y = y.to(device)
+            loop += 1
 
-            print(x.shape)
-            print(y.shape)
-            print(x.dtype)
-            print(y.dtype)
+            #x = x.to(device)
+            #y = y.to(device)
+            #print(x.shape)
+            #print(y.shape)
+            #print(x.dtype)
+            #print(y.dtype)
 
             optimizer.zero_grad()  # gradients do not have to be kept from last step
             
@@ -52,16 +74,19 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
             # with torch.set_grad_enabled(phase == 'train'):
             
             prediction = model(x)  # fwd pass
+            # prediction = prediction[:,0]
 
-            loss = criterion(prediction, y)
+            loss = criterion(prediction, y)  # loss is calculated batchsize times, and then averaged
             loss.backward()  # backprop
             optimizer.step()  # update params
-            train_loss += loss.item() * x.size(0)
-            
+            train_loss += loss.item() * x.size(0)  # loss*batchsize (as loss was averaged ('mean'))
 
+            if (loop%100) == 0:
+                print('loop: ', loop, '/', len(dloader_train), ' ... ', 'train loss (avg over these 100 loops): ', train_loss/100)
+                train_loss = 0
 
 model = models.resnet18(pretrained=True)
-model.to(device)
+#model.to(device)
 
 # do not apply backprop on weight that were used for feature extraction
 for param in model.parameters():
@@ -70,10 +95,10 @@ for param in model.parameters():
 # on these, backprop will be done
 n_features = model.fc.in_features
 model.fc = nn.Linear(n_features, N_CLASSES)  # modify fully connected layer (last layer)
-model = model.to(device)
+# model = model.to(device)
 
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.CrossEntropyLoss(reduction='mean')
 optimizer = torch.optim.Adam(model.fc.parameters(), lr= LEARNING_RATE)  # TODO ev add momentum
 #optimizer = torch.optim.Adam(model.parameters(), lr= LEARNING_RATE)
 
