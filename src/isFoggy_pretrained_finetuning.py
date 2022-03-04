03775b8615e20d9ab147a65577e91f1849a921f5
jbaumer <jbaumer@ethz.ch>
2022-03-04 18:17:06 +0100

    added validation (incl metrics)

--- src/isFoggy_pretrained_finetuning.py
+++ src/isFoggy_pretrained_finetuning.py
@@ -12,12 +12,36 @@ from torch import nn
 from torchvision import models
 from torch.utils.data import random_split
 from torch.optim import lr_scheduler
-from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import f1_score, confusion_matrix, accuracy_score, f1_score, recall_score, precision_score
 
 print('imports done')
 
 
 ################# FUNCTIONS ######################
+
+def get_and_print_stats(yt, yp, mode=None):
+    if mode == 'train':
+        print('TRAINING METRICS: ')
+    elif mode == 'val':
+        print('VALIDATION METRICS: ')
+
+    cm = confusion_matrix(y_true=yt, y_pred=yp)
+    print('confusion matrix: \n', cm, '\n')
+
+    accuracy = accuracy_score(y_true=yt, y_pred=yp)
+    print('accuracy \n', accuracy, '\n')
+
+    precision = precision_score(y_true=yt, y_pred=yp)
+    print('precision score \n', precision, '\n')
+
+    recall = recall_score(y_true=yt, y_pred=yp)
+    print('recall score \n', recall, '\n')
+
+    f1 = f1_score(y_true=yt, y_pred=yp)
+    print('f1 score \n', f1, '\n')
+
+    return cm, accuracy, precision, recall, f1
+
 
 def get_train_val_split(dset_full):
     print('splitting in train/test...')
@@ -27,24 +51,27 @@ def get_train_val_split(dset_full):
     dset_train, dset_val = random_split(dset_bb1, [len_train, len_val])  # Split Pytorch tensor
     return dset_train, dset_val
 
-def print_grid(x,y):
+def print_grid(x,y, batchsize):
+    x = x.cpu()
+    y = y.cpu()
     print(y)
-    grid_img = torchvision.utils.make_grid(x, nrow=4, normalize=True)
+    grid_img = torchvision.utils.make_grid(x, nrow=batchsize/2, normalize=True)
     plt.imshow(grid_img.permute(1, 2, 0))
     plt.show()
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs):
-    print('start training model...')
+def train_model(model, dloader, criterion, optimizer, scheduler, num_epochs):
+    print('start TRAINING model...')
     model = model.train()
-    since = time()
+    train_since = time()
 
     for epoch in range(num_epochs):
         print(f'{epoch}/{num_epochs}')
         print('-' * 10)
 
-        running_loss = 0.0  # loss (to be updated during loop)
-        running_corrects = 0  # correct labels (to be updated during loop)
+        running_loss, running_corrects = 0.0, 0  # loss (to be updated during loop)
         train_loss, epoch_loss = 0, 0
+        all_y_true, all_y_pred = [], []  # after a complete epoch, these two lists will have same length as dset_train
+
         loop = 0
 
         print('start training...')
@@ -59,7 +86,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
             """
             # plot at nth loop
             if loop == 26:
-                print_grid(x,y)
+                print_grid(x,y, BATCH_SIZE)
             """    
 
             optimizer.zero_grad()  # gradients do not have to be kept from last step
@@ -76,24 +103,84 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs):
 
             # statistics
             running_loss += loss.item() * x.size(0)  # loss*batchsize (as loss was averaged ('mean')), each item of batch had this loss on avg
+            running_corrects += torch.sum(prediction_binary == y).item()  #  correct predictions (of this batch)
+
+            y_true = y.cpu().tolist()
+            y_pred = prediction_binary.cpu().tolist()
+
+            all_y_true.extend(y_true)
+            all_y_pred.extend(y_pred)
+
             epoch_loss += loss.item() * x.size(0)
 
-            # add sth like: running_corrects += torch.sum(preds == labels.data)
 
 
             if (loop%200) == 0:
-                print('loop: ', loop, '/', len(dloader_train), ' ... ', 'train loss (avg over these 100 loops): ', train_loss/200)
+                print('loop: ', loop, '/', len(dloader_train), ' ... ', 'train loss (avg over these 200 loops): ', train_loss/200)
                 train_loss = 0
 
         print('epoch loss = ', epoch_loss/len(dloader_train))
-    
-    print('training time in seconds: ', time()-since)
+
+        cm, accuracy, precision, recall, f1 = get_and_print_stats(yt=all_y_true, yp=all_y_pred, mode='train')
+    # saving model
+    torch.save(obj=model, f=PATH_MODEL)
+    print('training time in seconds: ', time() - train_since)
+
+
+def val_model(model, dloader, criterion):
+    print('start VALIDATING the model...')
+    model = model.eval()
+    val_since = time()
+
+    print('-' * 10)
+
+    running_loss, running_corrects = 0.0, 0  # loss (to be updated during loop)
+    train_loss, epoch_loss = 0, 0
+    all_y_true, all_y_pred = [], []  # after a complete epoch, these two lists will have same length as dset_train
+
+    loop = 0
+
+    print('start validation loop...')
+    with torch.no_grad():
+        for x, y in dloader:  # x,y are already moved to device in dataloader
+            # move to GPU
+            x = x.to(device)
+            y = y.to(device)
+            y = y.float()
+            loop += 1
+
+            pred_probab = model(x)  # fwd pass
+            pred_probab_class_1 = pred_probab[:, 1]
+            prediction_binary = pred_probab.argmax(dim=1).float()
+
+            loss = criterion(pred_probab_class_1, y)  # loss is calculated batchsize times, and then averaged
+            #loss.backward()  # backprop
+            #optimizer.step()  # update params
+
+            # statistics
+            running_loss += loss.item() * x.size(0)  # loss*batchsize (as loss was averaged ('mean')), each item of batch had this loss on avg
+            running_corrects += torch.sum(prediction_binary == y).item()  #  correct predictions (of this batch)
+
+            y_true = y.cpu().tolist()
+            y_pred = prediction_binary.cpu().tolist()
+
+            all_y_true.extend(y_true)
+            all_y_pred.extend(y_pred)
+
+            epoch_loss += loss.item() * x.size(0)
+
+
+        print('epoch loss = ', epoch_loss/len(dloader_train))
+
+        cm, accuracy, precision, recall, f1 = get_and_print_stats(yt=all_y_true, yp=all_y_pred, mode='val')
+
+    print('validation time in seconds: ', time() - val_since)
 
 
 ################## REPRODUCIBILITY ##################
 
-torch.seed()
-torch.manual_seed(42)
+torch.seed()  # only for CPU
+torch.manual_seed(42)  # works for CPU and CUDA
 
 
 ############ ARGPARSERS, GLOBAL VARIABLES ############
@@ -103,24 +190,28 @@ parser.add_argument('--batch_size', help='batch size')
 parser.add_argument('--lr', help='learning rate')
 parser.add_argument('--epochs', help='number of training epochs')
 parser.add_argument('--train_split', help='train split')
+parser.add_argument('--station', help='station')
+parser.add_argument('--cam', help='camera number')
 
 args = parser.parse_args()
 
+STATION = args.station
+CAM = int(args.cam)
 BATCH_SIZE = int(args.batch_size)
 LEARNING_RATE = float(args.lr)
 EPOCHS = int(args.epochs)
 TRAIN_SPLIT = float(args.train_split)
 
 N_CLASSES = 2
+PATH_MODEL = f'models/{STATION}{CAM}_bs_{BATCH_SIZE}_LR_{LEARNING_RATE}_epochs_{EPOCHS}'
 
 
 # set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 # create datasets and dataloaders
-dset_bb1 = DischmaSet(root='../datasets/dataset_downsampled/', station='Buelenberg', camera=1)
-print('Dischma set (BB1) created.')
+dset_bb1 = DischmaSet(root='../datasets/dataset_downsampled_devel/', station='Buelenberg', camera=1)
+print(f'Dischma set {STATION}{CAM} created.')
 dset_bb1_train, dset_bb1_val = get_train_val_split(dset_bb1)
 dloader_train = DataLoader(dataset=dset_bb1_train, batch_size=BATCH_SIZE)
 dloader_val = DataLoader(dataset=dset_bb1_val, batch_size=BATCH_SIZE)
@@ -151,8 +242,6 @@ optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)  # Decay LR by a factor of 0.1 every 7 epochs
 """
 
-
-
-train_model(model=model, criterion=criterion, optimizer=optimizer, scheduler=None, num_epochs=EPOCHS)
-
+train_model(model=model, dloader=dloader_train, criterion=criterion, optimizer=optimizer, scheduler=None, num_epochs=EPOCHS)
+val_model(model=model, dloader=dloader_val, criterion=criterion)
 
