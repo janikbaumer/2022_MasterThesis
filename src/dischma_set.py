94f6596c5b01a80c645eab5de7a3e32443c9f1ca
jbaumer <jbaumer@ethz.ch>
2022-03-16 17:24:07 +0100

    save complete file path (for allowing training with data from multiple cams)

--- src/dischma_set.py
+++ src/dischma_set.py
@@ -66,11 +66,13 @@ class DischmaSet():
         # loop over all stations / cams (for devel, only choose BB1)
         # todo: when looping, consider that not all stations have 3 cameras
         # todo: when looping, maybe do nested list or change list name (depending on cam/station) -> consider that at same time, multiple images from diff cams exist
-        
+        self.root = root
+        self.stat_cam_lst = stat_cam_lst
+
         self.DOWNSCALE_FACTOR = 1  # 1 for no downsampling, else height and width is (each) downscaled by this factor
 
         self.is_foggy = []
-        self.file_list_valid = []
+        self.path_list_valid = []
 
         for camstation in stat_cam_lst:
             STATION, CAM = camstation.split('_')
@@ -93,44 +95,56 @@ class DischmaSet():
 
             # not done with dicts (keys cannot be accessed with indices (used for getitem method))
             for raw_img_name in self.file_list_camstat:  # raw_img_name: e.g. '20211230160501.jpg' or with .png
+                full_path = f'{self.PATH_RAW_IMAGE}/{raw_img_name}'
                 A0_img, fog_idx_img = get_indices(path=self.PATH_COMPOSITE, raw_img=raw_img_name)
                 if not (A0_img == None and fog_idx_img == None):  # if txt file for composite image generation did not exist (or did not contain row with name of image)
                     a0_passed_th, fog_passed_th = check_thresholds(A0_img, fog_idx_img, A0_optim, fog_idx_optim, A0_far0, fog_idx_far0)
                     used_for_comp = a0_passed_th and fog_passed_th  # one bool, whether this img was used for composite image generation
                     img_is_foggy = not(used_for_comp)  # image is considered as foggy if it was not used for the composite image generation
                     self.is_foggy.append(int(img_is_foggy))
-                    self.file_list_valid.append(raw_img_name)
-    
+                    # self.path_list_valid.append(raw_img_name)
+                    self.path_list_valid.append(full_path)
+
     def __len__(self):
         """
         returns the number of VALID samples in our dataset (where not both a0 and fog_idx are None)
         """
-        return len(self.file_list_valid)
+        return len(self.path_list_valid)
 
 
     def __getitem__(self, idx):
         """
         returns a data sample from the dataset (this fct will be called <batch size> times, in every loop)
         """
-
+        #### TODO CHECK HOW IMAGE IS GOTTEN
         # given idx, get image with filename belonging to this specific index
-        self.img_name = self.file_list_valid[idx]
-        self.path_img = f'{self.PATH_RAW_IMAGE}/{self.img_name}'
         
+        # self.img_name = self.path_list_valid[idx]
+        self.path_img = self.path_list_valid[idx]
+        
+
+        #for camstation in self.stat_cam_lst:
+        #    STATION, CAM = camstation.split('_')
+        #    if os.path.isfile(f'{self.root}DischmaCams/{STATION}/{CAM}/{self.img_name}'):  # make sure to read files from correct directory - TODO: what happens when two images are taken at exactly the same second on different cams/stations?
+        #        self.path_img = f'{self.root}{STATION}/{CAM}/{self.img_name}'  
+
+        # self.path_img = f'{self.PATH_RAW_IMAGE}/{self.img_name}'
+
         # read and downscale image
+        
         image = torchvision.io.read_image(path=self.path_img)
         shp = image.shape  # torch.Size([3, 6000, 4000])
         shp = tuple(image.shape[-2:])  # (6000, 4000)
         shp_new = tuple(int(x/self.DOWNSCALE_FACTOR) for x in shp)
         image = F.resize(img=image, size=shp_new)
         image = image.float()  # convert uint8 to float32
-        
+
         # get label (True if img is foggy, resp. img was not used for composite image generation)
         label = self.is_foggy[idx]
         # label = float(label)
 
         return image, label
-    
+
     def get_balancedness(self):
         n_clear = self.is_foggy.count(0)
         n_foggy = self.is_foggy.count(1)  # nmbr of images classified as foggy
