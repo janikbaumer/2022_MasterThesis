5628d26fd94088e5fd0495a7bf7a2304d71d19e3
jbaumer <jbaumer@ethz.ch>
2022-03-24 14:11:42 +0100

    program to manually classify images (with keystrokes)

---
+++ src/handlabelling.py
@@ -0,0 +1,101 @@
+# jbaumer@ethz.ch
+# 2022-03-24
+
+# manual classification of images of the Dischma valley (Davos)
+# if do with classification, make sure to end with button 'q' (do not interrupt program with Ctrl+C or similar !)
+
+import os
+import random
+import tkinter
+import cv2
+from tkinter import messagebox
+
+# may be removed
+random.seed(42)
+
+# no more needed (image will not even be shown, by checking 'data' variable)
+def write_to_file(string, textfile):
+    # appends the line only to the textfile if it has not yet been classified before
+    os.system(f"grep -q {string} {textfile} || echo {string} >> {textfile}")
+
+BASE_PATH = '../datasets/dataset_downsampled_devel/DischmaCams/'
+STATCAM_LST = [
+    'Buelenberg/1',
+    'Buelenberg/2',
+    'Giementaelli/1',
+    'Giementaelli/2',
+    'Giementaelli/3',
+    'Luksch/1',
+    'Luksch/2',
+    'Sattel/1',
+    'Sattel/2',
+    'Sattel/3',
+    'Stillberg/1',
+    'Stillberg/2',
+    'Stillberg/3'
+    ]
+CAMSTAT = 'Buelenberg/2'  # to choose (one of STATCAM_LST)
+
+ext = ('.png', '.jpg')
+manual_labels_file = os.path.join(BASE_PATH, CAMSTAT, 'manual_labels_isfoggy.txt')
+filelist = os.listdir(os.path.join(BASE_PATH, CAMSTAT))
+random.shuffle(filelist)
+
+# read whole txt file (to check later whether img has already been labelled)
+if os.path.isfile(manual_labels_file):
+    with open(file=manual_labels_file, mode='r') as txtfile:
+        data = txtfile.read()
+else:
+    data = ''
+
+with open(file=manual_labels_file, mode='a') as txtfile:
+    for file in filelist:
+        quit, cont = False, False
+        
+        # check if this image has already been classified manually (if line exists in txt file)
+        # if following two lines are uncommented, then images may be classified multiple times - manual_labels_isfoggy.txt files must be checked for duplicates
+        if file[0:14] in data:
+            continue
+
+        # label images of 2020 / 2021
+        # if image is (F)oggy, press button 'f'
+        # if image is (N)ot foggy, press button 'n'
+        # to quit, press button 'q' (do not only exit with x-symbol on top right)
+        if (file[0:4] == '2020' or file[0:4] == '2021') and file.endswith(ext):  # only label files of 2020/2021 
+            line = file + ' '
+            usage_string = 'Usage: \n f: (F)oggy \n n: (N)ot foggy \n q: (Q)uit'
+            img = cv2.imread(os.path.join(BASE_PATH, CAMSTAT, file))  # read a colour image from the working directory
+            cv2.imshow(f'{file} - {usage_string} - manually classifying {CAMSTAT} images', img)  # display the original image
+
+            key = cv2.waitKey(0) & 0xFF  # with param 0 (waitKey), the image is open till any key is pressed  /  0xFF only get last 8bits of the keys ( ord('...') )to accomodate in case NumLock is activated (or similar)
+
+            # KEYBOARD INTERACTIONS
+            if key == ord('f'):
+                line = line + '1\n'
+                txtfile.write(line)
+                #write_to_file(string=line, textfile=txtfile)
+                cv2.destroyAllWindows()
+
+            elif key == ord('n'):  
+                line = line + '0\n'
+                txtfile.write(line)
+                #write_to_file(string=line, textfile=txtfile)
+                cv2.destroyAllWindows()
+
+            elif key == ord('q'):
+                cv2.destroyAllWindows()
+                quit = True
+
+            else:  # error message for all other keys
+                root = tkinter.Tk()
+                messagebox.showinfo(title='Wrong Key Used', message=usage_string)
+                root.destroy()
+                cv2.destroyAllWindows()
+                cont = True
+
+        if quit == True:
+            break
+        if cont == True:
+            continue
+
+print('everything from years 2020 and 2021 classified, thanks :) ')
