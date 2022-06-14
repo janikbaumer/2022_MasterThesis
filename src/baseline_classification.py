24a900d5767a3c65da12eb274030d908f040a694
jbaumer <jbaumer@ethz.ch>
2022-06-14 18:58:08 +0200

    baseline for classification tasks (input one cam at a time)

---
+++ src/baseline_classification.py
@@ -0,0 +1,78 @@
+# jbaumer
+
+# calculate metrics for different datasets (1x/cam + 1 for all cams)
+
+from re import T
+import torch
+import torchvision
+import argparse
+import matplotlib.pyplot as plt
+import os
+import wandb
+import ast
+import random
+
+import numpy as np
+from time import time
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, recall_score, precision_score, precision_recall_curve
+
+from torch.utils.data import DataLoader, random_split
+from torch import nn
+from torchvision import models
+from torch.optim import lr_scheduler
+from tqdm import tqdm
+from dischma_set_classification import DischmaSet_classification
+
+print('imports done')
+
+############ REPRODUCIBILITY ############
+
+# Fix all random seeds
+random_seed = 42
+torch.seed()  # only for CPU
+torch.manual_seed(random_seed)  # should work for CPU and CUDA
+random.seed(random_seed)
+np.random.seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+os.environ['PYTHONHASHSEED'] = str(random_seed)
+
+
+############ ARGPARSERS ############
+
+parser = argparse.ArgumentParser(description='get baseline for classification task')
+parser.add_argument('--stations_cam', help='list of stations with camera number, separated with underscore (e.g. Buelenberg_1')
+parser.add_argument('--path_dset', help='path to used dataset ')
+
+args = parser.parse_args()
+
+# wandb.init(project="model_fog_classification", entity="jbaumer", config=args)
+
+############ GLOBAL VARIABLES ############
+
+PATH_DATASET = args.path_dset
+STATIONS_CAM_STR = args.stations_cam
+
+STATIONS_CAM_STR = STATIONS_CAM_STR.replace("\\", "")
+STATIONS_CAM_LST = sorted(ast.literal_eval(STATIONS_CAM_STR))  # sort to make sure not two models with data from same cameras (but input in different order) will be saved
+
+
+dset_test = DischmaSet_classification(root=PATH_DATASET, stat_cam_lst=STATIONS_CAM_LST, mode='test')
+dset_baseline = DischmaSet_classification(root=PATH_DATASET, stat_cam_lst=STATIONS_CAM_LST, mode='baseline')
+
+y_GT = dset_test.is_foggy
+y_pred_baseline = dset_baseline.is_foggy
+
+print('length of y GT: ', len(y_GT), 'length of y pred baseline: ', len(y_pred_baseline))
+acc = accuracy_score(y_GT, y_pred_baseline)
+prec = precision_score(y_GT, y_pred_baseline)
+rec = recall_score(y_GT, y_pred_baseline)
+f1 = f1_score(y_GT, y_pred_baseline)
+
+
+print('accuracy: ', acc)
+print('precision: ', prec)
+print('recall: ', rec)
+print('f1 score: ', f1)
