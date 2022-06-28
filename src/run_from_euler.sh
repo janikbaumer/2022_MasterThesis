1cf7c4808f1776bc30df8cc9a1b06a06c2b2b723
Janik Baumer <jbaumer@eu-login-49.euler.ethz.ch>
2022-06-28 12:32:37 +0200

    adapt template for personal usage

--- src/run_from_euler.sh
+++ src/run_from_euler.sh
@@ -1,22 +1,29 @@
-!/bin/bash
-
 #BSUB -W 48:00
 #BSUB -n 1
 #BSUB -o euleroutputs/outfile_%J.%I.txt
-#BSUB -R "rusage[mem=55000,ngpus_excl_p=1]"
+#BSUB -R "rusage[mem=15000,ngpus_excl_p=1]"
 #BSUB -R "select[gpu_mtotal0>=7500]"
 #BSUB -R "rusage[scratch=55000]"
-#BSUB -J "runname"
+#BSUB -J "testrun"
 
-#cp -r /cluster/work/igp_psr/jbaumer/stylebias/data/OfficeHome $TMPDIR/
-cp -r /cluster/scratch/jbaumer/datasets/ $TMPDIR
+echo "starting script from sh file"
+
+# echo "copying data to tmpdir..."
+# mkdir $TMPDIR/datasets
+# mkdir $TMPDIR/datasets/dataset_downsampled
+# cp -r /cluster/scratch/jbaumer/datasets/ $TMPDIR
+# echo "done copying data to tmpdir"
 
 echo TEMPDIR: $TMPDIR
 
 # activate Enviroment
 source ../venv/bin/activate
+echo "environment activated"
 
 # load modules
-module load gcc/8.2.0 gdal/3.2.0 zlib/1.2.9 eth_proxy hdf5/1.10.1
+# module load gcc/8.2.0 gdal/3.2.0 zlib/1.2.9 eth_proxy hdf5/1.10.1
+module load eth_proxy
+echo "modules loaded"
 
-python model_fog_classification.py  --batch_size 8 --lr 0.0001 --epochs 5 --train_split 0.8 --path_dset $TMPDIR/dataset_downsampled/ --stations \[\'Stillberg_1\'\] --weighted Manual --model resnet --optim SGD --lr_scheduler 15 --weight_decay 0.1 --momentum 0.9
+echo "starting actual python script..."
+python model_fog_classification.py  --batch_size 8 --lr 0.00001 --epochs 1 --train_split 0.8 --path_dset /cluster/scratch/jbaumer/datasets/dataset_downsampled/ --weighted False --model resnet --optim SGD --lr_scheduler 15 --weight_decay 0.1 --momentum 0.9  --stations "['Buelenberg_1']"
